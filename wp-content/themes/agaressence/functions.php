cc8b82cf7f6c2e65b2515a11fb7ae88f6a69756c
Nguyễn Văn Hoạch <theearthsmall@gmail.com>
2022-08-18 19:24:27 +0700

    u

--- wp-content/themes/agaressence/functions.php
+++ wp-content/themes/agaressence/functions.php
@@ -33,6 +33,7 @@
  *
  * @since Twenty Twenty 1.0
  */
+add_theme_support('woocommerce');
 function twentytwenty_theme_support() {
 
 	// Add default posts and comments RSS feed links to head.
@@ -786,15 +787,7 @@ function twentytwenty_get_elements_array() {
 	 */
 	return apply_filters( 'twentytwenty_get_elements_array', $elements );
 }
-//Thêm woocommerce
-// add_theme_support('woocommerce');
-add_action( 'after_setup_theme', 'woocommerce_support' );
-function woocommerce_support() {
-   add_theme_support( 'woocommerce' );
-} 
-if (class_exists('Woocommerce')){
-	add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
-}
+
 // function customtheme_add_woocommerce_support()
 //  {
 // add_theme_support( 'woocommerce' );
